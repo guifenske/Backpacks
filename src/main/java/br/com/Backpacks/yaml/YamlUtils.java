45f87c6a4c07584dd784cc1f339a82d4d46c3e48
3K <guifenske@gmail.com>
2023-11-27 22:49:27 -0300

    Refactor code to use UUID instead of Player object for backpack management, preventing some memory leaks
    Signed-off-by: 3K <guifenske@gmail.com>

--- src/main/java/br/com/Backpacks/yaml/YamlUtils.java
+++ src/main/java/br/com/Backpacks/yaml/YamlUtils.java
@@ -10,14 +10,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class YamlUtils {
 
-    public static void save_backpacks_yaml(Player player) throws IOException {
-        File file = new File(Main.back.getDataFolder().getCanonicalFile().getAbsolutePath() + "/" + player.getUniqueId() + ".yml");
+    public static void save_backpacks_yaml(UUID uuid) throws IOException {
+        File file = new File(Main.back.getDataFolder().getCanonicalFile().getAbsolutePath() + "/" + uuid + ".yml");
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
 
-        for(BackPack backPack : Main.back.backPackManager.getPlayerBackPacks(player)){
+        for(BackPack backPack : Main.back.backPackManager.getPlayerBackpacks(uuid)){
             config.set(backPack.getBackpack_id() + ".i", backPack.serialize());
             config.set(backPack.getBackpack_id() + ".1", backPack.getFirst_page().getStorageContents());
 
