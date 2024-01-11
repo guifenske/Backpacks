95f5a62512bb6dae1a24432a3c3356e4cef2a16f
3K <guifenske@gmail.com>
2024-01-10 22:34:43 -0300

    break the backpack correctly when it's an explosion
    Signed-off-by: 3K <guifenske@gmail.com>

--- src/main/java/br/com/backpacks/events/backpack_related/BackpackBreak.java
+++ src/main/java/br/com/backpacks/events/backpack_related/BackpackBreak.java
@@ -2,37 +2,51 @@ package br.com.backpacks.events.backpack_related;
 
 import br.com.backpacks.Main;
 import br.com.backpacks.backpackUtils.BackPack;
-import br.com.backpacks.recipes.RecipesNamespaces;
+import br.com.backpacks.recipes.Utils;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 
 public class BackpackBreak implements Listener {
 
     @EventHandler
-    private void generalBreakBackpackChestEvent(BlockBreakEvent event){
+    private void playerBreak(BlockBreakEvent event){
         if(!event.getBlock().getType().equals(Material.CHEST)) return;
         if(Main.backPackManager.getBackpackFromLocation(event.getBlock().getLocation()) == null) return;
 
         event.setDropItems(false);
 
         BackPack backPack = Main.backPackManager.getBackpackFromLocation(event.getBlock().getLocation());
-        ItemStack backpack_item = new ItemStack(Material.CHEST);
-        ItemMeta meta = backpack_item.getItemMeta();
-
-        meta.setDisplayName(backPack.getName());
-        meta.getPersistentDataContainer().set(new RecipesNamespaces().getIS_BACKPACK(), PersistentDataType.INTEGER, 1);
-        meta.getPersistentDataContainer().set(new RecipesNamespaces().getNAMESPACE_BACKPACK_ID(), PersistentDataType.INTEGER, backPack.getId());
-        meta.getPersistentDataContainer().set(backPack.getNamespaceOfBackpackType(), PersistentDataType.INTEGER, 1);
-        backpack_item.setItemMeta(meta);
+        ItemStack backpack_item = Utils.getItemFromBackpack(backPack);
 
         Location location = event.getBlock().getLocation();
         Main.backPackManager.getBackpacksPlacedLocations().remove(location);
+        Main.backPackManager.getBackpacks().put(backPack.getId(), backPack);
         event.getPlayer().getWorld().dropItemNaturally(location, backpack_item);
+    }
+
+    @EventHandler
+    private void explosionBreak(EntityExplodeEvent event){
+        for(Block block : event.blockList()){
+            if(!block.getType().equals(Material.CHEST)) continue;
+            if(Main.backPackManager.getBackpackFromLocation(block.getLocation()) == null) continue;
+
+            event.setCancelled(true);
+            block.setType(Material.AIR);
+
+            BackPack backPack = Main.backPackManager.getBackpackFromLocation(block.getLocation());
+            ItemStack backpack_item = Utils.getItemFromBackpack(backPack);
+
+            Location location = block.getLocation();
+            Main.backPackManager.getBackpacksPlacedLocations().remove(location);
+            Main.backPackManager.getBackpacks().put(backPack.getId(), backPack);
+            block.getWorld().dropItemNaturally(location, backpack_item);
+            break;
+       }
     }
 }
